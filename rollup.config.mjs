3b610611aed14c4cadee935b688f82134c33d85d
Stuart van Beek <art0rz@protonmail.com>
2023-02-22 11:44:39 +0100

    fix: improve rollup build config, add terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,31 @@
 import typescript from '@rollup/plugin-typescript';
-import dts from "rollup-plugin-dts";
+import dts from 'rollup-plugin-dts';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json' assert { type: 'json' };
 
 export default [{
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs'
-  },
-  plugins: [typescript()]
+	input: 'src/index.ts',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs'
+		},
+		{
+			file: pkg.module,
+			format: 'es' // the preferred format
+		},
+		{
+			file: pkg.browser,
+			format: 'iife',
+			name: 'FoxholeWarAPI' // the global which can be used in a browser
+		}
+	],
+	plugins: [typescript(), terser()]
 }, {
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs'
-  },
-  plugins: [dts()]
+	input: 'src/index.ts',
+	output: {
+		dir: 'dist',
+		format: 'cjs'
+	},
+	plugins: [dts()]
 }];
